9ce2d4733ec553a7a89bca8068db0a55ea54bfcc
Sin <88013497+DHsinn@users.noreply.github.com>
2026-01-30 11:33:51 +0900

    Implement stack operations in main.cpp
    29일날 푼 10828 문제 자리이동

--- solve/CPP/10828/main.cpp
+++ solve/CPP/10828/main.cpp
@@ -1,1 +1,47 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    int b[10000];
+    int bcount = 0;
+
+    string cmd;
+    for (int i = 0; i < N; i++) {
+        cin >> cmd;
+
+        if (cmd == "push") {
+            int x;
+            cin >> x;
+            b[bcount] = x;
+            bcount++;
+        }
+        else if (cmd == "pop") {
+            if (bcount == 0) {
+                cout << -1 << '\n';
+            } else {
+                cout << b[bcount - 1] << '\n';
+                bcount--;
+            }
+        }
+        else if (cmd == "size") {
+            cout << bcount << '\n';
+        }
+        else if (cmd == "empty") {
+            cout << (bcount == 0 ? 1 : 0) << '\n';
+        }
+        else if (cmd == "top") {
+            if (bcount == 0) cout << -1 << '\n';
+            else cout << b[bcount - 1] << '\n';
+        }
+    }
+
+    return 0;
+}
